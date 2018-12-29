ec87adc096e29a32bf75c5996728a4940853e04e
Marcin <onefanfare@gmail.com>
2018-12-28 20:53:47 -0500

    Tried to add basic bouncing.

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -2,20 +2,20 @@
 the program will know the acceleration, velocity, and 1d position
 of the ball particle, based on the initial conditions. 
 The user will be able to set the time intervals to calculate at."""
-float gravity = -9.81
-float velocity = 0
-float position = 0
-float time = 0
-float timeInterval = .01
-int iterations = 100
-bool changeDefaults = False
+gravity = -9.81
+otherForce = 0
+velocity = 9.81
+position = 0
+time = 0
+timeInterval = .01
+iterations = 100
+changeDefaults = True
 
 def printCurrentInfo():
-    print("current gravity: " + gravity + " current velocity: " + " current position: " + position + " at time: " + time)
-
+    print("current gravity: " + str(gravity) + " current velocity: " + str(velocity) + " current position: " + str(position) + " at time: " + str(time))
 def askForInput():
     position = float(input("What is the ball's starting position? (m) "))
-    if changeDefaults = true:
+    if changeDefaults == True:
         gravity = float(input("What is the system's gravity? (kgm/s^2) "))
         timeInterval = float(input("What is the time interval? (s) "))
         velocity = float(input("What is the ball's velocity? (m/s) " ))
@@ -24,7 +24,19 @@ def askForInput():
 askForInput()
 printCurrentInfo()
 
+while iterations > 0:
+        position = position + (velocity * timeInterval)
+        if position < 0 and velocity < 0:
+                velocity = -velocity
+        velocity = velocity + (gravity * timeInterval)
+        acceleration = gravity + otherForce
+        time = time + timeInterval
+        iterations = iterations - 1
+        if 0 == iterations%10:
+                printCurrentInfo()
+        
 
+printCurrentInfo()
     
 
 
