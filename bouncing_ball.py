6e9d291d2f74ff459151e9ab981c53775d9010c6
Marcin <onefanfare@gmail.com>
2019-01-07 06:52:01 -0500

    Final update to bouncing ball

--- bouncing_ball.py
+++ bouncing_ball.py
@@ -2,6 +2,8 @@
 the program will know the acceleration, velocity, and 1d position
 of the ball particle, based on the initial conditions. 
 The user will be able to set the time intervals to calculate at."""
+import time
+import os
 
 changeDefaults = True
 class Ball():
@@ -13,60 +15,69 @@ class Ball():
                 self.radius = radius
 
 
-class System():
+class Environment():
         def __init__(self, gravity, time, timeInterval, iterations, my_ball):
                 self.gravity = gravity
                 self.time = time
                 self.timeInterval = timeInterval
                 self.iterations = iterations
-        def bounce():
-                if (my_ball.position - my_ball.radius) < 0 and my_ball.velocity < 0:
-                        my_ball.velocity = -my_ball.velocity
-        def iterate():
-                bounce()
-                my_ball.position = my_ball.position + (timeInterval*my_ball.velocity)
-                my_ball.velocity = my_ball.velocity + (timeInterval*gravity)
-        def run_simulation():
-                while self.iterations > 0:
-                        my_ball.iterate()
-                        if self.iterations%10 == 0:
-                                printCurrentInfo()
-                        self.iterations = iterations - 1
-                self.time = self.time + timeInterval
         
 
 
 my_ball = Ball(0,10,1,1)
-my_system = System(-10, 0, .01, 100, my_ball)
+my_environment = Environment(-10, 0, .01, 100, my_ball)
 
 
 def askForInput():
     my_ball.position = float(input("What is the ball's starting position? (m) "))
     if changeDefaults == True:
-        my_system.gravity = float(input("What is the system's gravity? (kgm/s^2) "))
-        my_system.timeInterval = float(input("What is the time interval? (s) "))
+        my_environment.gravity = float(input("What is the Environment's gravity? (kgm/s^2) "))
+        my_environment.timeInterval = float(input("What is the time interval? (s) "))
         my_ball.velocity = float(input("What is the ball's velocity? (m/s) " ))
-        my_system.iterations = (float(input("How many seconds to run for? (s) "))/my_system.timeInterval)+1
+        my_environment.iterations = (float(input("How many seconds to run for? (s) "))/my_environment.timeInterval)+1
 
 def printCurrentInfo():
-    print("current gravity: " + str(my_system.gravity) + " current velocity: " + str(my_ball.velocity) + " current position: " + str(my_ball.position) + " at time: " + str(my_system.time))
+    print("current gravity: " + str(my_environment.gravity) + " current velocity: " + str(my_ball.velocity) + " current position: " + str(my_ball.position) + " at time: " + str(my_environment.time))
 
-def runSystem():
-        while my_system.iterations > 0:
-                if my_system.iterations%10 == 0:
-                        printCurrentInfo()
-                my_system.time = my_system.time + my_system.timeInterval
-                my_system.iterations = my_system.iterations - 1
-                my_ball.position = my_ball.position + my_ball.velocity*my_system.timeInterval
-                my_ball.velocity = my_ball.velocity + my_system.gravity*my_system.timeInterval
+def printGraph():
+        os.system('cls')
+        n = 26
+        while my_ball.position - n < 0:
+                if n % 5 == 0:
+                        print("| ", n)
+                else:
+                        print("|")
+                n = n - 1
+        print("o")
+        n = n - 1
+        while n > 0:
+                if n % 5 == 0:
+                        print("| ", n)
+                else:
+                        print("|")
+                n = n - 1
+        print("--0")
+        time.sleep(.1)
+
+def runEnvironment():
+        while my_environment.iterations > 0:
+                if my_environment.iterations%10 == 0:
+                        printGraph()
+                my_environment.time = my_environment.time + my_environment.timeInterval
+                my_environment.iterations = my_environment.iterations - 1
+                my_ball.position = my_ball.position + my_ball.velocity*my_environment.timeInterval
+                my_ball.velocity = my_ball.velocity + my_environment.gravity*my_environment.timeInterval
                 if my_ball.position < 0 and my_ball.velocity < 0:
                         my_ball.velocity = -my_ball.velocity
                 
 
+        
+
+
 
 askForInput()
 printCurrentInfo()
-runSystem()
+runEnvironment()
 
 
         
--- core_interpreter.py
+++
@@ -1,6 +0,0 @@
-def Hello_World():
-    print("Hello World!")
-
-Hello_World()
-
-
